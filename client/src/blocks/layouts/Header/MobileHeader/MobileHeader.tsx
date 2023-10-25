b6a5d363bce7a03dee1c3d00047dd807cf12382f
Igor` Hvoschevskiy <ihvoschevskiy@gmail.com>
2023-10-25 18:49:55 +0700

    Fix DEV-127: Header-mobile__menu-btn неверно отображается при закрытии модального окна

--- client/src/blocks/layouts/Header/MobileHeader/MobileHeader.tsx
+++ client/src/blocks/layouts/Header/MobileHeader/MobileHeader.tsx
@@ -18,12 +18,17 @@ export const MobileHeader: FC<IProps> = ({ className }) => {
     setIsShownModal(!isShownModal)
   }
 
+  const onCloseModal = () => {
+    setIsShownModal(false)
+    setIsOpen(false)
+  }
+
   return (
     <header className={cn('header-m', className)}>
       <Logo />
       <button onClick={onClickHandle} className="header-m__menu-btn">
         <MobileMenuIco type={isOpen === false ? 'open' : 'close'} className="header-m__menu-ico" />
-        <Modal isShown={isShownModal} isSuccess="mock" onClose={() => setIsShownModal(false)} />
+        <Modal isShown={isShownModal} isSuccess="mock" onClose={onCloseModal} />
       </button>
     </header>
   )
